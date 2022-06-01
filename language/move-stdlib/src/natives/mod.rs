ab8d1dc313b747f813ecc42a388edc198311f14a
Gökhan Şimşek <g.simsek@paralect.com>
2022-06-01 14:29:45 +0300

    Depend on exact libra commit (not branch)

--- language/move-stdlib/src/natives/mod.rs
+++ language/move-stdlib/src/natives/mod.rs
@@ -60,7 +60,7 @@ pub fn all_natives(move_std_addr: AccountAddress) -> NativeFunctionTable {
         ("VDF", "extract_address_from_challenge", ol_vdf::native_extract_address_from_challenge),
         ("Decimal", "demo", ol_decimal::native_demo),
         ("Decimal", "single", ol_decimal::native_single),
-        ("Decimal", "pair", ol_decimal::native_pair),        
+        ("Decimal", "pair", ol_decimal::native_pair),
     ];
     NATIVES
         .iter()
